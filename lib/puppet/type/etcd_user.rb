27096db353918f920f3e33d80731afaa64f975e0
dmaes <dieter.maes@dmaes.be>
2023-08-23 15:51:55 +0200

    update docs

--- lib/puppet/type/etcd_user.rb
+++ lib/puppet/type/etcd_user.rb
@@ -3,13 +3,15 @@
 Puppet::Type.newtype(:etcd_role_permission) do
   @doc = <<-DOC
     @summary
-      Manage an etcd role permissions
+      Manage etcd users.
+      This resource does not manage user passwords, since etcd doesn't provide the necessary endpoints to do this cleanly.
+      Users will be created without password (aka: only cert auth allowed), and manually configured passwords will be ignored.
   DOC
 
   ensurable
 
   newparam(:name, namevar: true) do
-    desc 'The name of the role permission. Must be of `${role}:${key}` format.'
+    desc 'The name of the user.'
   end
 
   newproperty(:roles, array_matching: :all) do
