9e798d1a47c2d69d1edaf407bd3bf238b3e40053
dmaes <dieter.maes@dmaes.be>
2023-08-23 08:51:32 +0200

    force simple output, undo debug

--- manifests/auth.pp
+++ manifests/auth.pp
@@ -7,30 +7,29 @@
 class etcd::auth {
   assert_private()
 
-  # Etcd_role {
-  #   before => Exec['etcd::auth'],
-  # }
+  Etcd_role {
+    before => Exec['etcd::auth'],
+  }
 
   create_resources('etcd_role', $etcd::roles)
 
   $env_list = $etcd::etcdctl_env.map |$key, $value| { "${key}='${value}'" }
   $env = join($env_list, ' ')
-  $etcdctl = "env ${env} etcdctl"
+  $etcdctl = "env ${env} ETCDCTL_WRITE_OUT='simple' etcdctl"
   $auth_disabled = "${etcdctl} auth status | grep 'Authentication Status: false'"
 
   notify { $auth_disabled: }
 
   if $etcd::auth {
-    exec { 'etcd::auth::enable':
+    exec { 'etcd::auth':
       path    => $facts['path'],
       command => "${etcdctl} auth enable",
       onlyif  => $auth_disabled,
     }
   } else {
-    exec { 'etcd::auth::disable':
+    exec { 'etcd::auth':
       path    => $facts['path'],
-      # command => "${etcdctl} auth disable",
-      command => "${etcdctl} auth status; /bin/false",
+      command => "${etcdctl} auth disable",
       unless  => $auth_disabled,
     }
   }
